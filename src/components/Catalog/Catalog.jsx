f84fe3f0d5e6f9fb8f457ac1db2497a3e8b1f558
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-06-05 16:30:51 +0300

    some styling

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -2,12 +2,16 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import CatalogCard from "./CatalogCard";
+import { Title, CardWrapper, CardCol } from "./styled";
+
+import CatalogCard from "./CatalogCard/CatalogCard";
 
 const API_URL = "https://60b8a5d4b54b0a0017c041b4.mockapi.io/api/v1/product/";
 
 function Catalog() {
   const [products, setProducts] = useState([]);
+  //const [basket, setBasket] = useState([]);
+  //const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
     axios
@@ -18,24 +22,28 @@ function Catalog() {
 
   return (
     <div>
-      <h3>Katalog</h3>
+      <Title>E-Commerce Site</Title>
 
       <div>
         <Link to="/details">Details</Link>
       </div>
-      {products.map((product) => {
-        return (
-          <div key={product._id}>
-            <CatalogCard
-              image={product.image}
-              title={product.title}
-              price={product.price}
-              category={product.category}
-              description={product.description}
-            />
-          </div>
-        );
-      })}
+      <CardWrapper>
+        {products.map((product) => {
+          return (
+            <div CardWey={product.id}>
+              <CardCol size={3}>
+                <CatalogCard
+                  image={product.image}
+                  title={product.title}
+                  price={product.price}
+                  category={product.category}
+                  description={product.description}
+                />
+              </CardCol>
+            </div>
+          );
+        })}
+      </CardWrapper>
     </div>
   );
 }
